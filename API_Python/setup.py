18dbe4da9b64b1c483c2452851a9cd42822d5860
Brian Bradley <embedit@embeditelectronics.com>
2015-01-19 16:29:14 -0500

    Adding setup script

---
+++ API_Python/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.2.5',
+      description='RPiSoC Python API',
+      author='Brian Bradley',
+      author_email='brian.bradley.p@gmail.com',
+      packages=['rpisoc'],
+     )
