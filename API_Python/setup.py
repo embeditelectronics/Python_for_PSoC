c2ea3a3b12d015828b0c3a69d0deaa974fd5c70e
Brian Bradley <brian.bradley.p@gmail.com>
2016-05-15 23:24:53 -0400

    Adding basic setup script to install the software.

---
+++ API_Python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+#from setuptools import setup
+
+setup(name='PiSoC',
+      version='2.0.1',
+      description='PiSoC Python API',
+      author='Brian Bradley',
+      author_email='bradley@embeditelectronics.com',
+      packages=['pisoc'],
+     )
